5e82e9863a176ad5a48a7f7b593e54c090d7681a
Guilherme <guichasemartinsgmr@hotmail.com>
2024-01-11 09:11:34 -0300

    clean

--- apps/authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ apps/authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -1,14 +1,7 @@
 package com.ead.authuser.dtos;
 
-import com.ead.authuser.enuns.UserStatus;
-import com.ead.authuser.enuns.UserType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
