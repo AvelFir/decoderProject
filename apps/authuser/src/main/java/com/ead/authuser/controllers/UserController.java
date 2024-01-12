a854df4e1c638bd3932005f2d6e90c6fe1447573
Guilherme <guichasemartinsgmr@hotmail.com>
2024-01-12 09:20:36 -0300

    feature: JsonView - Multiplas Visualizações em APIs

--- apps/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ apps/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserDTO;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -44,6 +48,58 @@ public class UserController {
         } else {
             userService.delete(userModelOptional.get());
             return ResponseEntity.status(HttpStatus.OK).body("User deleted");
+        }
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> updateUser(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView({UserDTO.UserView.UserPut.class}) UserDTO userDTO){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        if(userModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setFullName(userDTO.getFullName());
+            userModel.setPhoneNumber(userDTO.getPhoneNumber());
+            userModel.setCpf(userDTO.getCpf());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<?> updatePassword(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView({UserDTO.UserView.PasswordPut.class}) UserDTO userDTO){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        if(userModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } if(!userModelOptional.get().getPassword().equals(userDTO.getOldPassword())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password!");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setPassword(userDTO.getPassword());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully");
+        }
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<?> updateImage(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView({UserDTO.UserView.ImagePut.class}) UserDTO userDTO){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        if(userModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setImageURL(userDTO.getImageURL());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
     }
 }
--- apps/authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ apps/authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -1,18 +1,41 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class UserDTO {
 
+    public interface UserView{
+        public static interface RegistrationPost{}
+        public static interface UserPut{}
+        public static interface PasswordPut{}
+        public static interface ImagePut{}
+    }
+
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
+
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView(UserView.UserPut.class)
     private String fullName;
+
+    @JsonView(UserView.UserPut.class)
     private String phoneNumber;
+
+    @JsonView(UserView.UserPut.class)
     private String cpf;
+
+    @JsonView(UserView.ImagePut.class)
     private String imageURL;
 }
